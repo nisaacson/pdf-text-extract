00c675e6a7dcf947324e22fb68d8aa84f533366e
Noah Isaacson <noah@nisaacson.com>
2013-03-19 23:13:39 -0400

    move bufferedExtract into index

--- test/buffered-extract-test.js
+++ test/buffered-extract-test.js
@@ -2,14 +2,14 @@ var inspect = require('eyespect').inspector();
 var assert = require('assert')
 var fs = require('fs')
 var path = require('path')
-var bufferedExtract = require('../bufferedExtract.js')
+var extract = require('../index.js')
 var should = require('should');
 describe('Buffered Extract', function () {
   it('should extract text', function (done) {
     var desiredNumPages = 8
     var filePath = path.join(__dirname, 'data', 'multipage.pdf')
     assert.ok(fs.existsSync(filePath), 'pdf file not found at path: ' + filePath)
-    bufferedExtract(filePath, function (err, reply) {
+    extract(filePath, function (err, reply) {
       should.not.exist(err)
       should.exist(reply)
       done()
