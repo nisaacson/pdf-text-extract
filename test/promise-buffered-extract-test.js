53181af8e660fc941ed5d85268243837db5d7a7a
Huu Phuong NGUYEN <phuongnguyenhuu91@gmail.com>
2017-03-23 18:17:43 +0100

    Add promise support
    Add promise support - adjusted

---
+++ test/promise-buffered-extract-test.js
@@ -0,0 +1,24 @@
+var assert = require('assert')
+var fs = require('fs')
+var path = require('path')
+var Extract = require('../index.js')
+var should = require('should')
+
+describe('Buffered Extract Promise', function () {
+  it('should extract text', function (done) {
+    var desiredNumPages = 8
+    var filePath = path.join(__dirname, 'data', 'multipage.pdf')
+    assert.ok(fs.existsSync(filePath), 'pdf file not found at path: ' + filePath)
+    var extractor = new Extract(filePath)
+    extractor.then(function (err, pages) {
+      should.not.exist(err)
+      should.exist(pages, 'no pages extracted')
+      pages.length.should.eql(desiredNumPages)
+      pages.map(function (page) {
+        should.exist(page, 'page text content should exist')
+        page.length.should.be.above(0)
+      })
+      done()
+    })
+  })
+})
